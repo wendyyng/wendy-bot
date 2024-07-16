a78d8383e8f6d1051a125fb69a1c627686f530dc
wendyyng <ng_yih_wei@hotmail.com>
2024-07-16 22:30:40 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -43,11 +43,12 @@ if [ ! -f /etc/nginx/sites-available/myapp ]; then
     sudo bash -c 'cat > /etc/nginx/sites-available/myapp <<EOF
 server {
     listen 80;
-    server_name _;
+    server_name 3.89.191.113;
 
     location / {
         include proxy_params;
         proxy_pass http://unix:/var/www/wendy-bot/myapp.sock;
+        proxy_redirect off;
     }
 }
 EOF'
