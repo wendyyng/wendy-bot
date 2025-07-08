41e2de7f711561165d970c71639f5f6b69bb4ab4
wendyyng <ng_yih_wei@hotmail.com>
2025-07-08 14:13:57 +0800

    Change app.py to production

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def chat():
         return jsonify({"error": "An error occurred while processing your request."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
